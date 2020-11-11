045c18bd90a815cd72a92a2b4913ad7391bb1d7c
hong <asdwoo123@naver.com>
2020-11-11 09:26:14 +0900

    11-11

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import router from './router'
 import store from './store'
 import './plugins/ant-design-vue.js'
 import '@/styles/index.less'
+import AutoLaunch from 'auto-launch'
+
+const autoLauncher = new AutoLaunch({
+  name: 'seojuneng-application'
+})
+
+autoLauncher.isEnabled()
+    .then((isEnabled) => {
+      if (!isEnabled) autoLauncher.enable()
+    })
 
 Vue.config.productionTip = false
 
